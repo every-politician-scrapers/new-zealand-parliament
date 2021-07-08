46c690a23b771153862ab5ed57607554ff8c89bd
Tony Bowden <tony@tmtm.com>
2021-07-08 09:34:45 +0300

    initial scrapers and data

---
+++ bin/scraper/official.rb
@@ -0,0 +1,57 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'csv'
+require 'pry'
+require 'scraped'
+
+# TODO: fetch data from the individual member pages
+class Legislature
+  # details for an individual member
+  class Member < Scraped::HTML
+    field :id do
+      td[1].css('a/@href').text.split('/').last
+    end
+
+    field :name do
+      noko.css('noscript img/@alt').text
+    end
+
+    field :party do
+      td[2].text.tidy
+    end
+
+    field :electorate do
+      td[3].text.tidy
+    end
+
+    # This seems to be a 'last updated' rather than a start-date
+    # field :start_date do
+    # Date.parse(td[4].text.tidy).to_s
+    # end
+
+    private
+
+    def td
+      noko.css('td')
+    end
+  end
+
+  # The page listing all the members
+  class Members < Scraped::HTML
+    decorator Scraped::Response::Decorator::CleanUrls
+
+    field :members do
+      noko.css('.tab__body .table--list tr').drop(1).map { |mp| fragment(mp => Member).to_h }
+    end
+  end
+end
+
+url = 'https://www.parliament.nz/en/mps-and-electorates/members-of-parliament/'
+data = Legislature::Members.new(response: Scraped::Request.new(url: url).response).members
+
+header = data.first.keys.to_csv
+rows = data.map { |row| row.values.to_csv }
+abort 'No results' if rows.count.zero?
+
+puts header + rows.join
---
+++ bin/scraper/wikidata.rb
@@ -0,0 +1,72 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'cgi'
+require 'csv'
+require 'scraped'
+require 'pry'
+
+class Results < Scraped::JSON
+  field :members do
+    json[:results][:bindings].map { |result| fragment(result => Member).to_h }
+  end
+end
+
+class Member < Scraped::JSON
+  field :item do
+    json.dig(:member, :value).to_s.split('/').last
+  end
+
+  field :name do
+    json.dig(:name, :value)
+  end
+
+  # Wikidata => Official
+  PARTY_MAP = {
+    'ACT New Zealand' => 'ACT Party',
+    'Green Party of Aotearoa New Zealand' => 'Green Party',
+  }
+
+  field :party do
+    PARTY_MAP.fetch(partyLabel, partyLabel)
+  end
+
+  field :electorate do
+    json.dig(:areaLabel, :value).sub('List MP', 'List')
+  end
+
+  private
+
+  def partyLabel
+    json.dig(:partyLabel, :value)
+  end
+end
+
+WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql?format=json&query=%s'
+
+memberships_query = <<SPARQL
+  SELECT ?member ?name ?partyLabel ?areaLabel
+  WHERE {
+    ?member p:P39 ?ps .
+    ?ps ps:P39 wd:Q18145518 ; pq:P2937 wd:Q85738447 .
+    FILTER NOT EXISTS { ?ps pq:P582 ?end }
+    OPTIONAL { ?ps pq:P4100 ?party }
+    OPTIONAL { ?ps pq:P768 ?area }
+
+    OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
+    OPTIONAL { ?member rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
+    BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  }
+  ORDER BY ?name
+SPARQL
+
+url = WIKIDATA_SPARQL_URL % CGI.escape(memberships_query)
+headers = { 'User-Agent' => 'every-politican-scrapers/northern-ireland-assembly-official' }
+data = Results.new(response: Scraped::Request.new(url: url, headers: headers).response).members
+
+header = data.first.keys.to_csv
+rows = data.map { |row| row.values.to_csv }
+abort 'No results' if rows.count.zero?
+
+puts header + rows.join
